dd1ddaa0fc1ca29cb9777ea5bf850f1e2b720bd9
ZGS <zgstowell@gmail.com>
2022-03-17 11:37:51 -0700

    another deploy

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <div className={styles.container}>
         <Head>
           <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Zac Stowell Codes</title>
         </Head>
